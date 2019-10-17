c9637e2d8cf125a91d89f95d178bc47d53143d7b
micastamboliiska <56379875+micastamboliiska@users.noreply.github.com>
2019-10-17 15:19:41 +0300

    Create hello.cpp

---
+++ lab01/hello.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+ string facebook;
+ cout<<"Hello,world!"<<endl;
+ cout<<"You are in facebook? What is your name?";
+ cin>>facebook;
+ cout<<facebook;
+ return 0;
+}
